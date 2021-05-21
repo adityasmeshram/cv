8efb3a87cfd64beb110a682134f315dcab9f66fc
Aditya Meshram <adityasm@ghost.adityasm>
2021-05-21 17:48:54 +0530

    Updates

---
+++ precache-manifest.f9e2a06673910c9ae41c930d61db4252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf9311a903c368d885a",
+    "url": "/cv/static/css/main.8acb0ce4.chunk.css"
+  },
+  {
+    "revision": "3cf9311a903c368d885a",
+    "url": "/cv/static/js/main.67e31410.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "68f54d0df7a95b968d67",
+    "url": "/cv/static/js/2.d1804de4.chunk.js"
+  },
+  {
+    "revision": "fd9b6b23309f0d39f3a789849b59d0f4",
+    "url": "/cv/static/media/Aditya_Meshram_Resume.fd9b6b23.pdf"
+  },
+  {
+    "revision": "47757d76851231fae6df1cc2b300feca",
+    "url": "/cv/static/media/aditya.47757d76.jpg"
+  },
+  {
+    "revision": "2a8e33a5c66cdd80a4157c2688609922",
+    "url": "/cv/index.html"
+  }
+];
